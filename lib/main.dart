bf421c09b71c0634eae5c3d24641a55fd5bffb3a
senaybee <senay_berihu@outlook.com>
2020-07-15 20:16:34 -0700

    Extract first page widget with custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(FirstPage());
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(19, 19, 214, 1.0),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Random User Generator'),
         ),
         body: Center(
           child: RaisedButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                20.0,
+              ),
+            ),
             onPressed: () {},
             child: Container(
-              padding: EdgeInsets.all(4.0),
+              padding: EdgeInsets.all(8.0),
               child: Text(
                 'Generate a random user!',
                 style: TextStyle(fontSize: 18.0),
@@ -20,6 +35,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
