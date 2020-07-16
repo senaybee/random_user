001b7bfd124993d0c276e3781fb177cdbfce80e1
senaybee <senay_berihu@outlook.com>
2020-07-15 21:12:17 -0700

    Define Generator widget for the '/generator' route in the top level MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(FirstPage());
+  runApp(
+    MaterialApp(
+      theme: ThemeData(primaryColor: Color.fromRGBO(19, 19, 214, 1.0)),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstPage(),
+        '/generator': (context) => Generator(),
+      },
+    ),
+  );
 }
 
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(19, 19, 214, 1.0),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(19, 19, 214, 1.0),
+        title: Text('Random User Generator 🧑'),
+        centerTitle: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Random User Generator 🧑'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: RaisedButton(
-            color: Color.fromRGBO(19, 19, 214, 1.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                30.0,
-              ),
+      body: Center(
+        child: RaisedButton(
+          color: Color.fromRGBO(19, 19, 214, 1.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              30.0,
             ),
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                'Generate a random user!',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white,
-                ),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/generator');
+          },
+          child: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'Generate a random user!',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
               ),
             ),
           ),
@@ -44,9 +50,23 @@ class FirstPage extends StatelessWidget {
   }
 }
 
-class SecondPage extends StatelessWidget {
+class Generator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Here is your user! 💡'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello world!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        backgroundColor: Color.fromRGBO(19, 19, 214, 1.0),
+        onPressed: () {},
+      ),
+    );
   }
 }
