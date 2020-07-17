cb533957f8e03c44644c866667a5c4174870f5c7
senaybee <senay_berihu@outlook.com>
2020-07-16 23:02:02 -0700

    Display all relevant data fetched from API and resize image with round corners

--- lib/generator.dart
+++ lib/generator.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+//TODO: Create a file to hold const TextStyles
 
 class Generator extends StatefulWidget {
   @override
@@ -42,6 +43,7 @@ class _GeneratorState extends State<Generator> {
         child: Container(
             child: isLoaded
                 ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       ClipOval(
                         child: Image.network(
@@ -51,7 +53,12 @@ class _GeneratorState extends State<Generator> {
                           height: 200,
                         ),
                       ),
+                      Text(username),
+                      Text(password),
                       Text(name),
+                      Text(email),
+                      Text(phoneNumber),
+                      Text(birthday)
                     ],
                   )
                 : SpinKitRing(
@@ -87,6 +94,7 @@ class _GeneratorState extends State<Generator> {
         username = '${result['results'][0]['login']['username']}';
         password = '${result['results'][0]['login']['password']}';
         picture = '${result['results'][0]['picture']['large']}';
+        birthday = '${result['results'][0]['dob']['date']}'.substring(0, 10);
       });
     } else {
       throw Exception('Error fetching data');
